221df61b147de84f34a8a50544bf5b91547f81e2
vange <vanger2607@gmail.com>
2021-11-25 19:15:40 +0300

    I commit it again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QPushButton, QMainWindow,QApplication
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtWidgets import QPushButton, QMainWindow, QApplication
+from PyQt5.QtGui import QPainter, QPen, QColor
 from PyQt5.QtCore import Qt
 import sys
 from PyQt5 import uic
@@ -8,12 +8,41 @@ from random import randint
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class MyWidget(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(320, 260, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "круг"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
 
         super().__init__()
-        uic.loadUi('ui_file.ui', self)
+        self.setupUi(self)
         self.geometry()
 
         self.pushButton.clicked.connect(self.run)
@@ -32,10 +61,12 @@ class MyWidget(QMainWindow):
             qpp.end()
 
     def draw_circle(self, qp):
-        pen = QPen(Qt.yellow, 2)
+        pen = QPen(2)
+        color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+        pen.setColor(color)
         qp.setPen(pen)
-        qp.setBrush(Qt.yellow)
-        qp.drawEllipse(randint(100, 350), randint(100,350), randint(50,80), randint(50,80))
+        qp.setBrush(color)
+        qp.drawEllipse(randint(100, 350), randint(100, 350), randint(50, 80), randint(50, 80))
 
         self.to_do = False
 
